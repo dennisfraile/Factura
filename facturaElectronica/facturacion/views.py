736d6bd9879721dc11c744074667dd63fad2ed42
Dennis Fraile <dafsgamer3@outlook.es>
2024-04-18 16:13:11 -0600

    Se agregaron las views complementarias para los comprobantes de donacion

--- facturaElectronica/facturacion/views.py
+++ facturaElectronica/facturacion/views.py
@@ -91,7 +91,7 @@ class DepartamentoCreateView(UserPassesTestMixin,CreateView):
         pk=self.kwargs.get("pk")
         codigo = request.POST.get("codigo")
         valor = request.POST.get("valor")
-        pais= Pais.objects.create(codigo=codigo, valor=valor)
+        departamento= Departamento.objects.create(codigo=codigo, valor=valor)
         messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el departamento: "+ codigo + " " + valor + "con exito")
         return redirect('departamentoList')
 
@@ -254,8 +254,6 @@ class UnidadMedidaUpdateView(UserPassesTestMixin, UpdateView):
         UnidadMedida.objects.update(codigo=codigo, valor=valor)
         messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado la unidad de medida: "+ codigo + " " + valor + "con exito")
         return redirect('unidadMedidaList')
-
-
 
 
 class SujetoExcluidoMonthView(MonthArchiveView):
@@ -434,3 +432,324 @@ class ApendiceUpdateView(UserPassesTestMixin, UpdateView):
         Apendice.objects.update(campo=campo, etiqueta=etiqueta, valor=valor)
         messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el apendice: "+ campo + " " + valor + "con exito")
         return redirect('apendiceList')
+
+class TipoDocumentoView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'tipo_documento_view.html'
+    model = TipoDocumento
+    
+    def get_context_data(self, **kwargs) :
+        context = super(TipoDocumento, self).get_context(**kwargs)
+        tipoDocumento= TipoDocumento.objects.all()
+        context['registro'] = tipoDocumento
+        return context
+    
+class TipoDocumentoCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'tipo_documento_form.html'
+    form_class = TipoDocumentoForm
+    model = TipoDocumento
+    
+    def get_success_url(self):
+        return reverse_lazy('tipoDocumentoList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        codigo = request.POST.get("codigo")
+        valor = request.POST.get("valor")
+        tipoDocumento= TipoDocumento.objects.create(codigo=codigo, valor=valor)
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el tipo de documento : "+ codigo + " " + valor + "con exito")
+        return redirect('tipoDocumentoList')
+
+class TipoDocumentoUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'tipo_documento_form.html'
+    form_class = TipoDocumentoForm
+    
+    def get_success_url(self):
+        return reverse_lazy('tipoDocumentoList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        codigo = request.POST.get("codigo")
+        valor = request.POST.get("valor")
+        TipoDocumento.objects.update(codigo=codigo, valor=valor)
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el tipo de documento: "+ codigo + " " + valor + "con exito")
+        return redirect('tipoDocumentoList')
+
+class IdentificadorView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'identificador_view.html'
+    model = Identificador
+    
+    def get_context_data(self, **kwargs) :
+        context = super(Identificador, self).get_context(**kwargs)
+        identificador = Identificador.objects.all()
+        context['registro'] = identificador
+        return context
+    
+class IdentificadorCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'identificador_form.html'
+    form_class = IdentificadorForm
+    model = Identificador
+    
+    def get_success_url(self):
+        return reverse_lazy('identificadorList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        version = request.POST.get("version")
+        ambiente = request.POST.get("ambiente")
+        tipoDteId = request.POST.get("tipoDte")
+        tipoDte = TipoDocumento.objects.get(id=tipoDteId)
+        numeroControl = request.POST.get("numeroControl")
+        codigoGeneracion = request.POST.get("codigoGeneracion")
+        tipoModelo = request.POST.get("tipoModelo")
+        tipoOperacion = request.POST.get("tipoOperacion")
+        tipoContingencia = request.POST.get("tipoContingencia")
+        motivoContin = request.POST.get("motivoContin")
+        fechaEmi = request.POST.get("fechaEmi")
+        tipoMoneda = request.POST.get("tipoMoneda")
+        identificador= Identificador.objects.create(version=version, ambiente=ambiente, tipoDte=tipoDte, numeroControl=numeroControl,codigoGeneracion=codigoGeneracion, 
+                                                    tipoModelo=tipoModelo, tipoOperacion=tipoOperacion, tipoContingencia=tipoContingencia, motivoContin=motivoContin,
+                                                    fechaEmision=fechaEmi, tipoMoneda=tipoMoneda)
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el identificador: "+ version + " " + numeroControl + " " + codigoGeneracion + " "  + "con exito")
+        return redirect('identificadorList')
+
+class IdentificadorUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'identificador_form.html'
+    form_class = IdentificadorForm
+    
+    def get_success_url(self):
+        return reverse_lazy('identificadorList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        version = request.POST.get("version")
+        ambiente = request.POST.get("ambiente")
+        tipoDteId = request.POST.get("tipoDte")
+        tipoDte = TipoDocumento.objects.get(id=tipoDteId)
+        numeroControl = request.POST.get("numeroControl")
+        codigoGeneracion = request.POST.get("codigoGeneracion")
+        tipoModelo = request.POST.get("tipoModelo")
+        tipoOperacion = request.POST.get("tipoOperacion")
+        tipoContingencia = request.POST.get("tipoContingencia")
+        motivoContin = request.POST.get("motivoContin")
+        fechaEmi = request.POST.get("fechaEmi")
+        tipoMoneda = request.POST.get("tipoMoneda")
+        Identificador.objects.update(version=version, ambiente=ambiente, tipoDte=tipoDte, numeroControl=numeroControl,codigoGeneracion=codigoGeneracion, 
+                                                    tipoModelo=tipoModelo, tipoOperacion=tipoOperacion, tipoContingencia=tipoContingencia, motivoContin=motivoContin,
+                                                    fechaEmision=fechaEmi, tipoMoneda=tipoMoneda)
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el identificador: "+ version + " " + numeroControl + " " + codigoGeneracion + " "  + "con exito")
+        return redirect('identificadorList')
+
+class EmisorView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'emisor_view.html'
+    model = Emisor
+    
+    def get_context_data(self, **kwargs) :
+        context = super(Emisor, self).get_context(**kwargs)
+        emisor = Emisor.objects.all()
+        context['registro'] = emisor
+        return context
+    
+class EmisorCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'emisor_form.html'
+    form_class = EmisorForm
+    model = Emisor
+    
+    def get_success_url(self):
+        return reverse_lazy('emisorList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el emisor con exito")
+        return redirect('emisorList')
+
+class EmisorUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'emisor_form.html'
+    form_class = EmisorForm
+    model = Emisor
+    
+    def get_success_url(self):
+        return reverse_lazy('emisorList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el emisor con exito")
+        return redirect('emisorList')
+
+class ReceptorView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'receptor_view.html'
+    model = Receptor
+    
+    def get_context_data(self, **kwargs) :
+        context = super(Receptor, self).get_context(**kwargs)
+        receptor = receptor.objects.all()
+        context['registro'] = receptor
+        return context
+    
+class ReceptorCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'receptor_form.html'
+    form_class = ReceptorForm
+    model = Emisor
+    
+    def get_success_url(self):
+        return reverse_lazy('receptorList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el receptor con exito")
+        return redirect('recpetorList')
+
+class ReceptorUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'receptor_form.html'
+    form_class = ReceptorForm
+    model = Receptor
+    
+    def get_success_url(self):
+        return reverse_lazy('receptorList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el receptor con exito")
+        return redirect('receptorList')
+
+class OtroDocumentoAsociadoView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'otro_documento_asociado_view.html'
+    model = OtroDocumentoAsociado
+    
+    def get_context_data(self, **kwargs) :
+        context = super(OtroDocumentoAsociado, self).get_context(**kwargs)
+        otroDocumentoAsociado = OtroDocumentoAsociado.objects.all()
+        context['registro'] = otroDocumentoAsociado
+        return context
+    
+class OtroDocumentoAsociadoCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'otro_documento_asociado_form.html'
+    form_class = OtroDocumentoAsociadoForm
+    model = OtroDocumentoAsociado
+    
+    def get_success_url(self):
+        return reverse_lazy('otroDocumentoAsociadoList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el otro documento asociado con exito")
+        return redirect('otroDocumentoAsociadoList')
+
+class OtroDocumentoAsociadoUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'otro_documento_asociado_form.html'
+    form_class = OtroDocumentoAsociadoForm
+    model = OtroDocumentoAsociado
+    
+    def get_success_url(self):
+        return reverse_lazy('otroDocumentoAsociadoList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado otro documento asociado con exito")
+        return redirect('otroDocumentoAsociadoList')
+
+class CuerpoDocumentoView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'cuerpo_documento_view.html'
+    model = CuerpoDocumento
+    
+    def get_context_data(self, **kwargs) :
+        context = super(CuerpoDocumento, self).get_context(**kwargs)
+        cuerpoDocumento = CuerpoDocumento.objects.all()
+        context['registro'] = cuerpoDocumento
+        return context
+    
+class CuerpoDocumentoCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'cuerpo_documento_form.html'
+    form_class = CuerpoDocumentoForm
+    model = CuerpoDocumento
+    
+    def get_success_url(self):
+        return reverse_lazy('cuerpoDocumentoList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado el cuerpo del documento con exito")
+        return redirect('cuerpoDocumentoList')
+
+class CuerpoDocumentoUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'cuerpo_documento_form.html'
+    form_class = CuerpoDocumentoForm
+    model = CuerpoDocumento
+    
+    def get_success_url(self):
+        return reverse_lazy('cuerpoDocumentoList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado el cuerpo del documento con exito")
+        return redirect('cuerpoDocumentoList')
+
+class PagoDonacionView(View):
+    
+    login_url = '/ingresar/'
+    template_name = 'pago_donacion_view.html'
+    model = OtroDocumentoAsociado
+    
+    def get_context_data(self, **kwargs) :
+        context = super(PagoDonacion, self).get_context(**kwargs)
+        pagoDonacion = PagoDonacion.objects.all()
+        context['registro'] = PagoDonacion
+        return context
+    
+class PagoDonacionCreateView(UserPassesTestMixin,CreateView):
+    
+    login_url = '/ingresar'
+    template_name = 'pago_donacion_form.html'
+    form_class = PagoDonacionForm
+    model = PagoDonacion
+    
+    def get_success_url(self):
+        return reverse_lazy('pagoDonacionList')
+    
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a creado un pago a donacion con exito")
+        return redirect('pagoDonacionList')
+
+class PagoDonacionUpdateView(UserPassesTestMixin, UpdateView):
+    login_url = '/ingresar'
+    template_name = 'pago_donacion_form.html'
+    form_class = PagoDonacionForm
+    model = PagoDonacion
+    
+    def get_success_url(self):
+        return reverse_lazy('pagoDonacionList')
+
+    def post(self, request, *args, **kwargs):
+        pk=self.kwargs.get("pk")
+        messages.add_message(request=request, level=messages.SUCCESS, message= "Se a actualizado un pago a donacion con exito")
+        return redirect('pagoDonacionList')
